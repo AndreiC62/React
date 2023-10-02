0f3d077efb0964db1cd81c38f9fa8a2eb1f98cb1
AndreiC <andrei.cazac@yahoo.com>
2023-10-02 12:23:59 +0300

    started to work on this app

--- budget/src/App.js
+++ budget/src/App.js
@@ -1,5 +1,4 @@
-import "./App.css";
-import ExpenseItem from "./components/expenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -26,30 +25,8 @@ function App() {
 
   return (
     <div>
-      <h2>Let`s get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <h1 className="budget-app__name">AC budgetApp</h1>
+      <Expenses items={expenses} />
     </div>
   );
 }
